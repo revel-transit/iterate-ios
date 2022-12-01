56203bfc52112b2062a43e95f21ccb341cb7273e
Justin Savory <justin.savory@gorevel.com>
2022-12-01 03:02:02 -0800

    update spm spec

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "iteratehq",
+    platforms: [
+           .iOS(.v13), .tvOS(.v13)
+      ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,9 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "iteratehq",
-            dependencies: []),
-        .testTarget(
-            name: "iteratehqTests",
-            dependencies: ["iteratehq"]),
+            dependencies: [])
     ]
 )
