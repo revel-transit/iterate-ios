71850b4db8303c2340845da3a459244e1d63758f
Justin Savory <justin.savory@gorevel.com>
2022-12-01 03:28:31 -0800

    add back tests

---
+++ Tests/iteratehqTests/iteratehqTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import iteratehq
+
+final class iteratehqTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(iteratehq().text, "Hello, World!")
+    }
+}
